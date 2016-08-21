c0c5143d9d24eaf93a744f72b162e6f75ecfdca4
Tim Yousaf <timyousaf@gmail.com>
2016-08-21 17:29:11 -0400

    auth doesn't actually work.

--- app.py
+++ app.py
@@ -1,27 +1,19 @@
 import json
-from flask import Flask, url_for, redirect, session, render_template
-from flask_login import (UserMixin, login_required, login_user, logout_user,
-                         current_user)
-from flask_googlelogin import GoogleLogin
-from user import User
-
-users = {}
+from flask import Flask, url_for, redirect, session, render_template, jsonify
+from flask_oauth2_login import GoogleLogin
 
 app = Flask(__name__)
 app.config.update(
-    SECRET_KEY='Miengous3Xie5meiyae6iu6mohsaiRae',
-    GOOGLE_LOGIN_CLIENT_ID='209287169959-bbi3v6tpu99cb65ooos77c07gvmc7m4e.apps.googleusercontent.com',
-    GOOGLE_LOGIN_CLIENT_SECRET='BTlujArOQlOc01RXeoIpFbML',
-    GOOGLE_LOGIN_REDIRECT_URI='http://localhost:5000/oauth2callback')
-googlelogin = GoogleLogin(app)
-
-@googlelogin.user_loader
-def get_user(userid):
-    return users.get(userid)
+  SECRET_KEY="Miengous3Xie5meiyae6iu6mohsaiRae",
+  GOOGLE_LOGIN_REDIRECT_SCHEME="http",
+  GOOGLE_LOGIN_CLIENT_ID="209287169959-bbi3v6tpu99cb65ooos77c07gvmc7m4e.apps.googleusercontent.com",
+  GOOGLE_LOGIN_CLIENT_SECRET="BTlujArOQlOc01RXeoIpFbML"
+)
+google_login = GoogleLogin(app)
 
 @app.route("/charts")
-@login_required
 def charts():
+    print google_login.session()
     return render_template('charts.html')
 
 @app.route("/api/data")
@@ -37,51 +29,19 @@ def data():
     ];
     return json.dumps(response)
 
-@app.route('/')
+@app.route("/")
 def index():
-    return """
-        <p><a href="%s">Login</p>
-        <p><a href="%s">Login with extra params</p>
-        <p><a href="%s">Login with extra scope</p>
-    """ % (
-        googlelogin.login_url(approval_prompt='force'),
-        googlelogin.login_url(approval_prompt='force',
-                              params=dict(extra='large-fries')),
-        googlelogin.login_url(
-            approval_prompt='force',
-            scopes=['https://www.googleapis.com/auth/drive'],
-            access_type='offline',
-        ),
-    )
+  return """
+<html>
+<a href="{}">Login with Google</a>
+""".format(google_login.authorization_url())
 
-@app.route('/profile')
-@login_required
-def profile():
-    return """
-        <p>Hello, %s</p>
-        <p><img src="%s" width="100" height="100"></p>
-        <p>Token: %r</p>
-        <p>Extra: %r</p>
-        <p><a href="/logout">Logout</a></p>
-        """ % (current_user.name, current_user.picture, session.get('token'),
-               session.get('extra'))
+@google_login.login_success
+def login_success(token, profile):
+  return jsonify(token=token, profile=profile)
 
-@app.route('/oauth2callback')
-@googlelogin.oauth2callback
-def login(token, userinfo, **params):
-    user = users[userinfo['id']] = User(userinfo)
-    login_user(user)
-    session['token'] = json.dumps(token)
-    session['extra'] = params.get('extra')
-    return redirect(params.get('next', url_for('.profile')))
-
-@app.route('/logout')
-def logout():
-    logout_user()
-    session.clear()
-    return """
-        <p>Logged out</p>
-        <p><a href="/">Return to /</a></p>
-        """
+@google_login.login_failure
+def login_failure(e):
+  return jsonify(error=str(e))
 
 app.run(debug=True)
