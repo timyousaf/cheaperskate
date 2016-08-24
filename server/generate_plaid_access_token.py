1a74c59dd40732b5dc2240e98c37bcd165d1a25b
Tim Yousaf <timyousaf@gmail.com>
2016-08-23 21:44:12 -0400

    simplified access token script.

--- server/generate_plaid_access_token.py
+++ server/generate_plaid_access_token.py
@@ -4,32 +4,48 @@ from plaid.utils import json
 from optparse import OptionParser
 import sys
 
-parser = OptionParser()
-parser.add_option("-f", "--file", dest="filename",
-                  help="Specify the file from which to load credentials.")
-(options, args) = parser.parse_args()
+# parser = OptionParser()
+# parser.add_option("-f", "--file", dest="filename",
+#                   help="Specify the file from which to load credentials.")
+# (options, args) = parser.parse_args()
 
-if options.filename:
-	try:
-		creds_file =  open(filename)
-	except:
-		print("Failed to open file. Please pass a valid filename with -f.")
-		sys.exit()
-else:
-	print("Please specify a filename with -f.")
-	sys.exit()
+# if options.filename:
+# 	try:
+# 		creds_file =  open(filename)
+# 	except:
+# 		print("Failed to open file. Please pass a valid filename with -f.")
+# 		sys.exit()
+# else:
+# 	print("Please specify a filename with -f.")
+# 	sys.exit()
 
-creds = json.loads(creds_file.read())
-client_id = creds["client_id"]
-secret = creds["secret"]
-account_type = creds["account_type"]
-username = creds["username"]
-password = creds["password"]  	
+
+client_id = raw_input("Please enter your client id: ")
+secret = raw_input("Please enter your secret: ")
 
 Client.config({
 	'url': 'https://tartan.plaid.com'
 })
 client = Client(client_id=client_id, secret=secret)
+
+try:
+    institutions = client.institutions().json()
+    valid_account_types = []
+    for institution in institutions:
+    	valid_account_types.append(institution['type'])
+except plaid_errors.PlaidError as e:
+	print e.message
+	sys.exit()
+
+account_type = raw_input("Please enter the account type (institution) :")
+
+if account_type not in valid_account_types:
+	print("{0} is not a valid account. These are the valid accounts: {1}"
+		.format(account_type, json.dumps(valid_account_types)))
+	sys.exit()
+
+username = raw_input("Please enter your username: ")
+password = raw_input("Please enter your password: ")
 		
 try:
     response = client.connect(
@@ -43,4 +59,10 @@ except plaid_errors.PlaidError as e:
 	sys.exit()
 else:
 	connect_data = response.json()
-	print connect_data['access_token']
+	access_token = connect_data['access_token']
+	print "Your access token is {}".format(access_token)
+	print "Try retrieving transactions with this curl command:"
+	print "curl -X POST https://tartan.plaid.com/connect/get -d client_id={0} -d secret={1} -d access_token={2}".format(
+		client_id,
+		secret,
+		access_token)
