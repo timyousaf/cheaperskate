9fe4ad3a57c64cdbb9d4c6bbc00ced39425231b1
Tim Yousaf <timyousaf@gmail.com>
2016-12-22 18:05:15 -0600

    refactor

--- mint_elk_pipeline.py
+++ mint_elk_pipeline.py
@@ -7,43 +7,61 @@ from elasticsearch import Elasticsearch
 backup_filename = "/Users/timyousaf/.cheaperskate.mint.cache"
 es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
 
-def indexMintTransactions(account):
-
-	email = account["email"]
-	password = account["password"]
-	ius_session = account["ius_session"]
-	thx_guid = account["thx_guid"]
-
-	print "Deleting ElasticSearch index ..."
-	es.indices.delete(index='test-index', ignore=[400, 404])
+class MintPipeline():
 	
-	print "Querying Mint ..."
-	mint = mintapi.Mint(email, password, ius_session, thx_guid)
-	transactions = mint.get_transactions()
+	accounts = {}
 
-	print "Received transactions. Caching ..."
-	with open(backup_filename, 'wb') as output:
-		pickle.dump(transactions, output, -1)
+	def loadMintAccount(self, creds):
+		email = creds["email"]
+		password = creds["password"]
+		ius_session = creds["ius_session"]
+		thx_guid = creds["thx_guid"]
+		owner = creds["owner"]
 
-	print "Indexing transactions in ElasticSearch ..."
-	with open(backup_filename, 'rb') as input:
-		transactions = pickle.load(input)
+		print "Querying Mint ..."
+		mint = mintapi.Mint(email, password, ius_session, thx_guid)
+		transactions = mint.get_transactions()
 		transactions = json.loads(transactions.to_json(orient='records', date_format='iso'))
+		
+		print "Received transactions for {0}".format(email)
+		for transaction in transactions:
+			  transaction["owner"] = owner
+
+		return transactions
+
+	def saveCache(self):
+		print "Caching data ..."
+		with open(backup_filename, 'wb') as output:
+			pickle.dump(accounts, output, -1)
+
+	def loadCache(self):
+		print "Loading data from cache ..."
+		with open(backup_filename, 'rb') as input:
+			self.accounts = pickle.load(input)
+
+	def __init__(self, creds_file):
+		lines = open(creds_file)
+		for line in lines:
+			try:
+				creds = json.loads(line)
+				self.accounts[creds["email"]] = self.loadMintAccount(creds)
+			except Exception as e:
+				print("Failed on this Mint account: {}".format(line))
+				print e
+				sys.exit()
+
+	def index(self):
+		print "Deleting ElasticSearch index ..."
+		es.indices.delete(index='test-index', ignore=[400, 404])
 
 		id = 0
-		for transaction in transactions:
-		  es.index(index='mint', doc_type='transaction', id=id, body=transaction)
-		  id += 1
-		  if id % 100 is 0: print "Indexed {0} transactions ...".format(id)
+		for account in self.accounts:
+			transactions = self.accounts[account]
+			for transaction in transactions:
+			  es.index(index='mint', doc_type='transaction', id=id, body=transaction)
+			  id += 1
+			  if id % 100 is 0: print "Indexed {0} transactions ...".format(id)
 
-	print "Done!"
-
-creds_file = open("/Users/timyousaf/mint.txt")
-for line in creds_file:
-	try:
-		account = json.loads(line)
-		indexMintTransactions(account)
-	except Exception as e:
-		print("Failed on this Mint account: {}".format(line))
-		print e
-		sys.exit()
+pipeline = MintPipeline("/Users/timyousaf/mint.txt")
+pipeline.index()
+print "Finished!"
